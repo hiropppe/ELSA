078463464bc68ed3edcb454a11837147e0c6b638
hiropppe <sergei.0910@gmail.com>
2019-05-28 17:21:01 +0900

    fixing

---
+++ script/process_raw_tweet/tweet_token2.py
@@ -0,0 +1,48 @@
+import click
+import json
+
+from pathlib import Path
+from collections import Counter
+from create_vocab import VocabBuilder
+from word_generator import TweetWordGenerator
+
+from tqdm import tqdm
+
+
+@click.argument("input_path")
+@click.argument("output_dir")
+@click.option("--lang", "-lang", default="ja", help="")
+def main(input_path, output_dir, lang):
+    token_output = Path(output_dir).joinpath('elsa_{:s}_tokens.txt'.format(lang)).as_posix()
+    emoji_output = Path(output_dir).joinpath('elsa_{:s}_emoji.txt'.format(lang)).as_posix()
+
+    emoji_unicodes = json.loads(open("./emoji_unicode", "r").read())
+    emoji_unicode = emoji_unicodes.keys()
+
+    with open(input_path, 'r', encoding='utf8') as fi:
+        wg = TweetWordGenerator(fi)
+
+        n_sents, n_emoji, n_emoji_sents = 0, 0, 0
+        emoji_freq = Counter()
+
+        with open(token_output, 'w') as fot:
+            for i, (tokens, info) in tqdm(enumerate(wg)):
+                n_sents += 1
+                emoji_sent = 0
+                for token in tokens:
+                    if token in emoji_unicode:
+                        n_emoji += 1
+                        emoji_sent = 1
+                        emoji_freq[token] += 1
+                n_emoji_sents += emoji_sent
+                print(json.dumps(tokens), file=fot)
+
+        print(n_sents, n_emoji, n_emoji_sents, float(n_emoji_sents)/n_sents)
+
+    with open(emoji_output, "w", 'utf-8') as foe:
+        for token, freq in emoji_freq.most_common(n=len(emoji_freq)):
+            print("{:s}\t{:s}".format(token, freq), file=foe)
+
+
+if __name__ == '__main__':
+    main()
